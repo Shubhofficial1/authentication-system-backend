94220e370b1be5f59a02a5747167f1dab8659d69
shubham kumar <shubhofficial1@gmail.com>
2025-03-31 04:08:55 +0530

    Feat: Basic routing setup

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -24,4 +24,39 @@ const health = (req, res, next) => {
     }
 };
 
-export { self, health };
+const register = (req, res) => {
+    res.send('Register Endpoint');
+};
+
+const confirmation = (req, res) => {
+    res.send('Confirmation Endpoint');
+};
+
+const login = (req, res) => {
+    res.send('Login Endpoint');
+};
+
+const selfIdentification = (req, res) => {
+    res.send('Self Identification Endpoint');
+};
+
+const logout = (req, res) => {
+    res.send('Logout Endpoint');
+};
+
+const refreshToken = (req, res) => {
+    res.send('refreshToken Endpoint');
+};
+
+const forgotPassword = (req, res) => {
+    res.send('forgotPassword Endpoint');
+};
+
+const resetPassword = (req, res) => {
+    res.send('resetPassword Endpoint');
+};
+const changePassword = (req, res) => {
+    res.send('changePassword Endpoint');
+};
+
+export { self, health, register, confirmation, login, selfIdentification, logout, refreshToken, forgotPassword, resetPassword, changePassword };
--- src/router/apiRouter.js
+++ src/router/apiRouter.js
@@ -1,9 +1,30 @@
 import { Router } from 'express';
-import { self, health } from '../controller/apiController.js';
+import {
+    self,
+    health,
+    register,
+    confirmation,
+    login,
+    selfIdentification,
+    logout,
+    refreshToken,
+    forgotPassword,
+    resetPassword,
+    changePassword
+} from '../controller/apiController.js';
 import rateLimit from '../middleware/rateLimit.js';
 
 const router = Router();
 router.route('/self').get(rateLimit, self);
 router.route('/health').get(rateLimit, health);
+router.route('/register').post(rateLimit, register);
+router.route('/confirmation/:token').put(rateLimit, confirmation);
+router.route('/login').post(rateLimit, login);
+router.route('/self-identification').get(rateLimit, selfIdentification);
+router.route('/logout').post(rateLimit, logout);
+router.route('/refresh-token').post(rateLimit, refreshToken);
+router.route('/forgot-password').put(rateLimit, forgotPassword);
+router.route('/reset-password/:token').put(rateLimit, resetPassword);
+router.route('/change-password').put(rateLimit, changePassword);
 
 export default router;
